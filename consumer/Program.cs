2124cfc6408a84e9a42ac7c35b9a5ef195226f72
Luke Bakken <luke@bakken.io>
2022-09-15 13:57:20 -0700

    Everything works without TLS

--- consumer/Program.cs
+++ consumer/Program.cs
@@ -1,23 +1,69 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+
 using System.Text;
 
-var factory = new ConnectionFactory() { HostName = "rabbitmq" };
-using var connection = factory.CreateConnection();
-using var channel = connection.CreateModel();
+AutoResetEvent latch = new AutoResetEvent(false);
 
-channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
-
-Console.WriteLine(" [*] Waiting for messages.");
-
-var consumer = new EventingBasicConsumer(channel);
-consumer.Received += (model, ea) =>
+void CancelHandler(object? sender, ConsoleCancelEventArgs e)
 {
-    var body = ea.Body.ToArray();
-    var message = Encoding.UTF8.GetString(body);
-    Console.WriteLine($" [x] Received {message}");
-};
-channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+    Console.WriteLine("CTRL-C pressed, exiting!");
+    e.Cancel = true;
+    latch.Set();
+}
 
-Console.WriteLine(" Press [enter] to exit.");
-Console.ReadLine();
+Console.CancelKeyPress += new ConsoleCancelEventHandler(CancelHandler);
+
+Console.WriteLine("CONSUMER: waiting 5 seconds to try initial connection");
+Thread.Sleep(TimeSpan.FromSeconds(5));
+
+var factory = new ConnectionFactory() { HostName = "rabbitmq" };
+
+bool connected = false;
+
+IConnection? connection = null;
+
+while(!connected)
+{
+    try
+    {
+        connection = factory.CreateConnection();
+        connected = true;
+    }
+    catch (BrokerUnreachableException)
+    {
+        connected = false;
+        Console.WriteLine("CONSUMER: waiting 5 seconds to re-try connection!");
+        Thread.Sleep(TimeSpan.FromSeconds(5));
+    }
+}
+
+using (connection)
+{
+    if (connection == null)
+    {
+        Console.Error.WriteLine("CONSUMER: unexpected null connection");
+    }
+    else
+    {
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            Console.WriteLine("CONSUMER: waiting for messages...");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received {message}");
+            };
+
+            channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+            latch.WaitOne();
+        }
+    }
+}
--- producer/Program.cs
+++ producer/Program.cs
@@ -1,18 +1,52 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+
 using System.Text;
 
+Console.WriteLine("PRODUCER: waiting 5 seconds to try initial connection");
+Thread.Sleep(TimeSpan.FromSeconds(5));
+
 var factory = new ConnectionFactory() { HostName = "rabbitmq" };
-using var connection = factory.CreateConnection();
-using var channel = connection.CreateModel();
 
-channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+bool connected = false;
 
-string message = "Hello World!";
-var body = Encoding.UTF8.GetBytes(message);
+IConnection? connection = null;
 
-while(true)
+while(!connected)
 {
-    channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
-    Console.WriteLine($" [x] Sent {message}");
-    Thread.Sleep(TimeSpan.FromSeconds(5));
+    try
+    {
+        connection = factory.CreateConnection();
+        connected = true;
+    }
+    catch (BrokerUnreachableException)
+    {
+        connected = false;
+        Console.WriteLine("PRODUCER: waiting 5 seconds to re-try connection!");
+        Thread.Sleep(TimeSpan.FromSeconds(5));
+    }
+}
+
+using (connection)
+{
+    if (connection == null)
+    {
+        Console.Error.WriteLine("PRODUCER: unexpected null connection");
+    }
+    else
+    {
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        string message = "Hello World!";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        while (true)
+        {
+            channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+            Console.WriteLine($" [x] Sent {message}");
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+        }
+    }
 }
